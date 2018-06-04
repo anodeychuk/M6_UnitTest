3bb349162c5fad34a85286314a08117800bfa833
Andrii Odejchuk <36604414+anodeychuk@users.noreply.github.com>
2018-06-04 13:49:26 +0300

    Add files via upload

---
+++ UnitTestProject/CollectionTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using M6_UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class CollectionTest
+    {
+        [TestMethod]
+        public void ListAllItemsAreUniqueTest()
+        {
+            // create object
+            Teacher Tom = new Teacher("Tom Paterson", 38);
+
+            CollectionAssert.AllItemsAreUnique(Tom.Skill);
+        }
+
+        [TestMethod]
+        public void ListContainsTest()
+        {
+            // create object
+            Student Ivan = new Student("Ivan Pupkin", 22, 110);
+            Ivan.Learning("C#", 100);
+
+            CollectionAssert.Contains(Ivan.Skill, "C#");
+        }
+
+        [TestMethod]
+        public void TypeTest()
+        {
+            // create object
+            Worker Slu = new Worker("Slu Porter", 35 );
+
+            Assert.IsInstanceOfType(Slu , typeof(Person) );
+        }
+    }
+}
